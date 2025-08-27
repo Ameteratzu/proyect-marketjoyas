47a1d5fd5e2cf49fe7a7a0142d5860196f8a5444
Yber-M <ybermarlon@gmail.com>
2025-08-27 13:02:49 -0500

    STYLE: se agrego estilo de hover al nav

--- src/features/header/components/NavLink.tsx
+++ src/features/header/components/NavLink.tsx
@@ -13,11 +13,10 @@ export default function NavLink(props: Props) {
   const inactiveCls = "text-primary hover:text-accent-warm";
 
   const underline =
-    "after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:bg-accent-warm after:transition-all after:duration-300";
+    "after:content-[''] after:absolute after:left-1/2 after:-bottom-1 after:h-[2px] after:bg-accent-warm after:transition-transform after:duration-300 after:-translate-x-1/2 after:origin-center";
 
-  const underlineActive = "after:w-full"; // subrayado completo fijo
-
-  const underlineInactive = "after:w-0 hover:after:w-full"; // animación de expandir
+  const underlineActive = "after:w-full after:scale-x-100";
+  const underlineInactive = "after:w-full after:scale-x-0 hover:after:scale-x-100";
 
   const cls = cn(
     base,
