50ea48dbe02da8ae9b5c2b6861135fa855a676d8
Yber-M <ybermarlon@gmail.com>
2025-08-27 14:41:25 -0500

    ADD: funcionalidad para que suba al top al cambiar secciones

---
+++ src/routes/ScrollToTop.tsx
@@ -0,0 +1,11 @@
+// Sirve para que al cambiar de ruta, la página se desplace hacia arriba
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+  return null;
+}
