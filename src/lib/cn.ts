71569f6319b8e746bb892146807370c44ab87f5a
Yber-M <ybermarlon@gmail.com>
2025-08-27 10:08:47 -0500

    CHANGE

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,10 +1,12 @@
+// Lib para concatenar clases condicionalmente
+// Ejemplo: cn('class1', condition && 'class2', isActive ? 'class3' : 'class4')
 export function cn() {
-    const classes = [];
-    for (let i = 0; i < arguments.length; i++) {
-        const v = arguments[i];
-        if (typeof v === "string" && v) {
-            classes.push(v);
-        }
+  const classes = [];
+  for (let i = 0; i < arguments.length; i++) {
+    const v = arguments[i];
+    if (typeof v === "string" && v) {
+      classes.push(v);
     }
-    return classes.join(" ");
+  }
+  return classes.join(" ");
 }
