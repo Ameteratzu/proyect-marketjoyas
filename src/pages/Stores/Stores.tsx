c27638b3a49d09d0e3f28850680ef5cef05f7506
Yber-M <ybermarlon@gmail.com>
2025-08-27 14:29:44 -0500

    ADD: carpetas creadas y archivos de prueba para menejar rutas

---
+++ src/pages/Stores/Stores.tsx
@@ -0,0 +1,10 @@
+export default function Stores() {
+  return (
+    <section className="container-p py-12">
+      <h1 className="h1 text-3xl">Nuestras tiendas</h1>
+      <p className="lead mt-3">
+        Encuentra nuestras tiendas físicas en diversas ubicaciones.
+      </p>
+    </section>
+  );
+}
