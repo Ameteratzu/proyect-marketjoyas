0d4bd327fe98476cf835ae2209e5724cc006a171
Yber-M <ybermarlon@gmail.com>
2025-08-28 13:02:27 -0500

    ADD: se define los grupos de enlaces

---
+++ src/features/footer/footer.const.ts
@@ -0,0 +1,24 @@
+// Esto sirve para definir los grupos de enlaces que se mostrarán en el pie de página del sitio web.
+import type { FooterGroup } from "./footer.types";
+
+export const FOOTER_GROUPS: FooterGroup[] = [
+  {
+    id: "account",
+    titleKey: "account.title",
+    items: [
+      { labelKey: "account.profile", href: "/cuenta" },
+      { labelKey: "account.store", href: "/mi-tienda" },
+      { labelKey: "account.cart", href: "/carrito" },
+      { labelKey: "account.privacy", href: "/legal/politica-privacidad" },
+    ],
+  },
+  {
+    id: "info",
+    titleKey: "info.title",
+    items: [
+      { labelKey: "info.about", href: "/nosotros" },
+      { labelKey: "info.refund", href: "/legal/reembolsos" },
+      { labelKey: "info.claimBook", href: "/legal/libro-reclamaciones" },
+    ],
+  },
+];
