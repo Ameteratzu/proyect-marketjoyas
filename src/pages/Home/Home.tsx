561ecb862310b762b1058dba92a8dbd97b7994e3
Yber-M <ybermarlon@gmail.com>
2025-09-04 11:40:56 -0500

    ADD: se agrega el componente de la seccion de tinedas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import HeroCarousel from "@/pages/Home/components/HeroCarousel/HeroCarousel";
 import JewelryCategories from "@/pages/Home/components/JewelryCategories/JewelryCategories";
+import StoresSection from "@/pages/Home/components/StoresSection/StoresSection";
 
 export default function Home() {
   return (
@@ -7,6 +8,9 @@ export default function Home() {
       <HeroCarousel />
       {/* Sección: Joyas */}
       <JewelryCategories />
+
+      {/* Sección: Tiendas */}
+      <StoresSection />
     </>
   );
 }
