7e17b80306d4e7e68108474a7bf106587e05977d
Yber-M <ybermarlon@gmail.com>
2025-09-05 12:42:59 -0500

    ADD: se quito la importacion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,6 @@ import HeroCarousel from "@/pages/Home/components/HeroCarousel/HeroCarousel";
 import JewelryCategories from "@/pages/Home/components/JewelryCategories/JewelryCategories";
 import StoresSection from "@/pages/Home/components/StoresSection/StoresSection";
 import Personalized from "./components/Personalized/Personalized";
-import ScrollTopBtn from "@/components/ScrollTopBtn";
 
 export default function Home() {
   return (
