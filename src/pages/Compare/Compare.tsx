c808f27e49a2d1040b5e66527c4af41a578e1f2f
Yber-M <ybermarlon@gmail.com>
2025-09-08 11:34:36 -0500

    ADD: componente de stock in stock

--- src/pages/Compare/Compare.tsx
+++ src/pages/Compare/Compare.tsx
@@ -1,8 +1,12 @@
+import StockBadge from "./components/StockBadge";
+
 export default function Compare() {
     return (
         <section className="container-p py-12">
           <h1 className="h1 text-3xl">Prueba del Comparar</h1>
           <p className="lead mt-3">Acá podrás comparar las joyas y elegir la que más te guste</p>
+          <StockBadge inStock={false} />
+          <StockBadge inStock={true} />
         </section>
       );
 }
