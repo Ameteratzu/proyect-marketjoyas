c599058dc69f3a268aa5112deb841f2375a87e34
Yber-M <ybermarlon@gmail.com>
2025-09-09 12:15:07 -0500

    ADD: se termino con el apartado de En todo el pais

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,15 @@
 import HeroAbout from "./components/HeroAbout";
+import SpecialtiesGrid from "./components/SpecialtiesGrid";
+import BandCta from "./components/BandCta";
+import StoryBlock from "./components/StoryBlock";
+
 export default function About() {
   return (
+    <div >
       <HeroAbout />
+      <SpecialtiesGrid />
+      <BandCta />
+      <StoryBlock />
+    </div>
   );
 }
