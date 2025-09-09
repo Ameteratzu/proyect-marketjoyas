be08c61233c87ef738453b23e9ada290151e852b
Yber-M <ybermarlon@gmail.com>
2025-09-09 10:46:16 -0500

    ADD: se termino el hero

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,6 @@
+import HeroAbout from "./components/HeroAbout";
 export default function About() {
   return (
-    <section className="container-p py-12">
-      <h1 className="h1 text-3xl">Prueba del Sobre Nosotros</h1>
-      <p className="lead mt-3">Aqui sabrás todo sobre nuestra empresa</p>
-    </section>
+      <HeroAbout />
   );
 }
