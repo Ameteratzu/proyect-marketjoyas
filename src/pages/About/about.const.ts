6e5072285dcb59743795879af5e4bcc29f3465f9
Yber-M <ybermarlon@gmail.com>
2025-09-19 10:52:56 -0500

    FIX: se usa @ para traer las imagenes

--- src/pages/About/about.const.ts
+++ src/pages/About/about.const.ts
@@ -1,7 +1,12 @@
 // Definimos aca las constantes y tipos relacionados con las especialidades
+import img1 from "@/assets/about/Especialidad1.png";
+import img2 from "@/assets/about/Especialidad2.png";
+import img3 from "@/assets/about/Especialidad3.png";
+import img4 from "@/assets/about/Especialidad4.png";
+
 export type Specialty = {
   id: string;
-  image?: string;
+  image: string;
   titleKey: string;
   descKey: string;
 };
@@ -9,25 +14,25 @@ export type Specialty = {
 export const SPECIALTIES: Specialty[] = [
   {
     id: "clients",
-    image: "/src/assets/about/Especialidad1.png",
+    image: img1,
     titleKey: "specialties.clients.title",
     descKey: "specialties.clients.desc",
   },
   {
     id: "shipping",
-    image: "/src/assets/about/Especialidad2.png",
+    image: img2,
     titleKey: "specialties.shipping.title",
     descKey: "specialties.shipping.desc",
   },
   {
     id: "deals",
-    image: "/src/assets/about/Especialidad3.png",
+    image: img3,
     titleKey: "specialties.deals.title",
     descKey: "specialties.deals.desc",
   },
   {
     id: "brands",
-    image: "/src/assets/about/Especialidad4.png",
+    image: img4,
     titleKey: "specialties.brands.title",
     descKey: "specialties.brands.desc",
   },
